1fc42ae4dc1ca7c9dd8d624cc2a3372791e94c61
kamal-marouane <110986028+kamal-marouane@users.noreply.github.com>
2023-05-26 07:55:27 +0100

    our commit for jenkins

---
+++ src/main/java/ensias/mobilitymanagement/mobility/model/TestingCICD.java
@@ -0,0 +1,7 @@
+package ensias.mobilitymanagement.mobility.model;
+
+public class TestingCICD {
+    public static void main(String[] args) {
+        System.out.println("Hello Kamal, Hiba, Nabila and Fati");
+    }
+}
